62a574917598669a37d3dcb64596e90bc770f2b4
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2020-01-21 14:36:03 -0300

    Ciclo de vida de los componentes en Angular

--- platzi-store/src/app/components/product.component.ts
+++ platzi-store/src/app/components/product.component.ts
@@ -1,17 +1,46 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+    Component,
+    Input,
+    Output,
+    EventEmitter,
+    OnChanges,
+    OnDestroy,
+    SimpleChanges,
+    OnInit, DoCheck
+} from '@angular/core';
 import { Product } from '../product.model';
 
 @Component({
     selector: 'app-product',
     templateUrl: './product.component.html'
 })
-export class ProductComponent {
-
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
 
     @Input() product: Product;
     @Output() productClicked: EventEmitter<any> = new EventEmitter();
 
-    addCart(){
+    constructor() {
+        console.log('1. constructor');
+    }
+
+    // ngOnChanges(changes: SimpleChanges) {
+    //     console.log('2. ngOnChanges');
+    //     console.log(changes);
+    // }
+
+    ngOnInit() {
+        console.log('3. ngOnInit');
+    }
+
+    ngDoCheck() {
+        console.log('4. DoCheck');
+    }
+
+    ngOnDestroy() {
+        console.log('5. DoCheck');
+    }
+
+    addCart() {
         console.log('Añadir al carrito');
         this.productClicked.emit(this.product.id);
     }
