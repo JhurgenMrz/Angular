161a8a70e34af217f54f3ca269c3f2e8e5f9c16a
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2020-01-21 18:12:24 -0300

    Directivas

---
+++ platzi-store/src/app/hightlight.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appHightlight]'
+})
+export class HightlightDirective {
+
+  constructor(
+    element: ElementRef
+  ) {
+    element.nativeElement.style.backgroundColor = 'red';
+  }
+
+}
