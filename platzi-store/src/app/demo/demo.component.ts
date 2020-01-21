17e6894fca7b57961d1b04eeeaac1363e793acba
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2020-01-21 18:58:31 -0300

    Creando rutas en angular

---
+++ platzi-store/src/app/demo/demo.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.scss']
+})
+export class DemoComponent implements OnInit {
+  title = 'platzi-store';
+
+  items = ['Jhurgen', 'Gonza', 'Marco'];
+
+  power = 10;
+
+  addItem() {
+    this.items.push(this.title);
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
