a66ae5d03fe5fac047d386b22b5f2c8e4eb17de1
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2020-01-24 12:54:24 -0300

    Deploy v0.2

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/platzi-store'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/platzi-store/index.html'));
+app.use(cors());
+app.use(express.static(__dirname + "/dist/platzi-store"));
+app.get("*", function(req, res) {
+	res.sendFile(path.join(__dirname + "/dist/platzi-store/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
